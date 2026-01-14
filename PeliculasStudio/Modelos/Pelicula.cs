f386fd2253320b41bf55955e8884877787292cb1
royalthi13 <royalthi13@gmail.com>
2026-01-14 12:20:59 +0100

    Modelos de Peliculas y Usuarios Finalizados

---
+++ PeliculasStudio/Modelos/Pelicula.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+
+namespace PeliculasStudio.Modelos
+{
+   public  class Pelicula
+    {
+        [PrimaryKey , AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Titulo { get; set; }
+
+        [NotNull]
+        public string Resumen { get; set; }
+
+        [NotNull]
+        public string Genero { get; set; }
+
+        [NotNull]
+
+        public  int Anio { get; set; }
+
+        [NotNull]
+        public string TrailerPath { get; set; }
+
+        [NotNull]
+        public string PortadaPath { get; set; }
+
+
+
+    }
+}
---
+++ PeliculasStudio/Modelos/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace PeliculasStudio.Modelos
+{
+    public class Usuario
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull, Unique]
+        public string Nombreusuario { get; set; }
+
+        [NotNull]
+        public string Contrasenia { get; set; }
+
+        [NotNull]
+        public string Rol { get; set; }
+    }
+}
